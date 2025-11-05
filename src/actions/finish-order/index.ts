a46bced472e617e33fd74fc5cda3af193b533790
Tiago Lima <tiagolima@osf.digital>
2025-11-04 22:51:12 -0300

    feat: ensure stripe session is created.

--- src/actions/finish-order/index.ts
+++ src/actions/finish-order/index.ts
@@ -1,7 +1,6 @@
 "use server";
 
 import { eq } from "drizzle-orm";
-import { revalidatePath } from "next/cache";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -47,6 +46,7 @@ export const finishOrder = async () => {
     0,
   );
 
+  let orderId: string | undefined;
   await db.transaction(async (tx) => {
     if (!cart.shippingAddress) {
       throw new Error("Shipping address not found");
@@ -77,6 +77,7 @@ export const finishOrder = async () => {
       throw new Error("Failed to create order");
     }
 
+    orderId = order.id;
     const orderItemsPayload: Array<typeof orderItemTable.$inferInsert> =
       cart.items.map((item) => ({
         orderId: order.id,
@@ -89,4 +90,10 @@ export const finishOrder = async () => {
     await tx.delete(cartTable).where(eq(cartTable.id, cart.id));
     await tx.delete(cartItemTable).where(eq(cartItemTable.cartId, cart.id));
   });
+
+  if (!orderId) {
+    throw new Error("Failed to create order");
+  }
+
+  return { orderId };
 };
